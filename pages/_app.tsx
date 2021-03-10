caa13e3d8b4168f0d9ec3f6bbdb496e50db66cc1
AndreMoura <andre.moura@kinghost.com.br>
2021-03-10 09:53:55 -0300

    Criado base da aplicaçao

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import { GlobalStyle } from '../app/styles/globals';
+function MyApp({ Component, pageProps }) {
+  return (
+  <>
+    <Component {...pageProps} />
+    <GlobalStyle />
+  </>
+  )
+}
+
+export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Start</h1>
+    </div>
+  )
+}
