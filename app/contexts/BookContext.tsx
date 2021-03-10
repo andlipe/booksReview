bfd3bb1024c0b9a04326b4c89524f1da88d4e6f6
AndreMoura <andre.moura@kinghost.com.br>
2021-03-10 16:39:40 -0300

    Criado contexto de Livros

---
+++ app/contexts/BookContext.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { IBook } from "../types/IBooks";
+
+interface BooksProviderProps {
+    children: React.ReactNode;
+    books: Array<IBook>;
+}
+
+interface BookContextData {
+    books: Array<IBook>;
+}
+
+export const BooksContext = React.createContext({} as BookContextData);
+
+export function BooksProvider({children, ...rest}: BooksProviderProps) {
+    const [books, setBooks] = React.useState([]);
+
+    React.useEffect(() => {
+        setBooks(rest.books)
+    },[])
+    
+    return (
+
+        <BooksContext.Provider 
+        value={{
+            books
+        }}>
+            {children}
+        </BooksContext.Provider>
+    )
+}
