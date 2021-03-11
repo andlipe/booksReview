2634336a20c63d4880dc1fad10f82af3c3c00a17
AndreMoura <andre.moura@kinghost.com.br>
2021-03-11 19:54:15 -0300

    Criado logica para api de livros retornar eles de forma ordenada pelo edition

--- pages/api/books.ts
+++ pages/api/books.ts
@@ -2,17 +2,43 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import books from '../../livros.json';
 import { IBook } from "../../app/types/IBooks";
 
-type IResponse = {
-  results: Array<IBook>
-}
-
 type IMessage = {
   message: string;
 }
 
-export default (req: NextApiRequest, res: NextApiResponse<IResponse | IMessage>) => {
+export default (req: NextApiRequest, res: NextApiResponse<Array<IBook> | IMessage>) => {
   if (req.method === 'GET') {
-    return res.status(200).json(books);
+    let meses = [
+      "Janeiro",
+      "Fevereiro",
+      "Março",
+      "Abril",
+      "Maio",
+      "Junho",
+      "Julho",
+      "Agosto",
+      "Setembro",
+      "Outubro",
+      "Novembro",
+      "Dezembro"
+    ];
+
+    let bookList = books.results;
+
+    bookList.sort((a,b) => {
+      let separateMonthAndYearA = a.edition.split(' de ');
+      let yearFromA = separateMonthAndYearA[1];
+      let monthFromA = meses.findIndex((mes) => mes === separateMonthAndYearA[0])
+      let dateA = +new Date(Number(yearFromA), monthFromA+1);
+
+      let separateMonthAndYearB = b.edition.split(' de ');
+      let yearFromB = separateMonthAndYearB[1];
+      let monthFromB = meses.findIndex((mes) => mes === separateMonthAndYearB[0])
+      let dateB = +new Date(Number(yearFromB), monthFromB+1);
+      return dateB - dateA
+    })
+
+    return res.status(200).json(bookList);
   }
     return res.status(401).json({ message: "Method not allowed"});
   
